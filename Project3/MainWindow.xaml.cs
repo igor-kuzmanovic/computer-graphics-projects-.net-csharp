da0b02b7f3fb710d4a084322e87fdd995f7b50b0
igor-kuzmanovic <igor.kuzmanovic.996@gmail.com>
2019-02-18 08:18:59 +0100

    Initial commit

--- Project3/MainWindow.xaml.cs
+++ Project3/MainWindow.xaml.cs
@@ -20,9 +20,45 @@ namespace Project3
     /// </summary>
     public partial class MainWindow : Window
     {
+        const int networkGridColumns = 267;
+        const int networkGridRows = 150;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Random random = new Random();
+            byte[] rgbValues = new byte[3];
+
+            ICollection<Rectangle> rectangles = new List<Rectangle>(networkGridColumns * networkGridRows);
+            for (int i = 0; i < networkGridColumns; i++)
+            {
+                for (int j = 0; j < networkGridRows; j++)
+                {
+                    random.NextBytes(rgbValues);
+                    Color color = Color.FromRgb(rgbValues[0], rgbValues[1], rgbValues[2]);
+
+                    Rectangle rectangle = new Rectangle();
+                    rectangle.Fill = new SolidColorBrush(color);
+                    rectangle.SetValue(Grid.ColumnProperty, i);
+                    rectangle.SetValue(Grid.RowProperty, j);
+                    NetworkGrid.Children.Add(rectangle);                  
+                }
+            }
+
+            for (int i = 0; i < networkGridColumns; i++)
+            {
+                ColumnDefinition column = new ColumnDefinition();
+                column.Width = new GridLength(1, GridUnitType.Star);
+                NetworkGrid.ColumnDefinitions.Add(column);
+            }
+
+            for (int i = 0; i < networkGridRows; i++)
+            {
+                RowDefinition row = new RowDefinition();
+                row.Height = new GridLength(1, GridUnitType.Star);
+                NetworkGrid.RowDefinitions.Add(row);
+            }
         }
     }
 }
