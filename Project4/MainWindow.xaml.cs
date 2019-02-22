c43542f109b015ea6bc90e54c83f057ebf8e576e
igor-kuzmanovic <igor.kuzmanovic.996@gmail.com>
2019-02-22 11:35:11 +0100

    Project 4 - finished

--- Project4/MainWindow.xaml.cs
+++ Project4/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,16 @@ namespace Project4
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Point start = new Point();
+        private Point diffOffset = new Point();
+        private double startAngleX = 0;
+        private double startAngleY = 0;
+        private int zoomCurrent = 1;
+        private int zoomMax = 7;
+        private GeometryModel3D hitGeo;
+        private MapNetworkModel mapNetworkModel;
+        private Dictionary<GeometryModel3D, string> toolTips;
+
         public MainWindow()
         {
             InitializeComponent();         
@@ -32,66 +43,66 @@ namespace Project4
             NetworkModelUTM networkModelUTM = dataHelper.ImportNetworkModelUTMFromXML();
             NetworkModelGeo networkModelGeo = dataHelper.ConvertNetworkModelUTMToGeo(networkModelUTM);
             dataHelper.ExportNetworkModelGeoToXml(networkModelGeo);
-            MapNetworkModel mapNetworkModel = dataHelper.ConvertNetworkModelGeoToMap(networkModelGeo);
+            mapNetworkModel = dataHelper.ConvertNetworkModelGeoToMap(networkModelGeo);
             dataHelper.ExportMapNetworkModelToXml(mapNetworkModel);
-            Draw3DLines(mapNetworkModel);
-            Draw3DPoints(mapNetworkModel);
+            Draw3DLines();
+            Draw3DPoints();
         }
 
-        private void Draw3DPoints(MapNetworkModel mapNetworkModel)
+        private void Draw3DPoints()
         {
+            toolTips = new Dictionary<GeometryModel3D, string>();
             mapNetworkModel.Points.ForEach(point =>
             {
-                int side = 6;
-                int height = 0;
-                if (point.Type == PointType.Connector)
+                if (point.Type != PointType.Connector)
                 {
-                    side = 2;
-                    height = 2;
-                }
+                    int side = 6;
+                    int height = 0;
 
-                GeometryModel3D model = new GeometryModel3D();
-                model.Geometry = new MeshGeometry3D()
-                {
-                    Positions = new Point3DCollection()
+                    GeometryModel3D model = new GeometryModel3D();
+                    model.Geometry = new MeshGeometry3D()
                     {
-                        new Point3D(point.X + side, point.Y + side, height + side),
-                        new Point3D(point.X, point.Y + side, height + side),
-                        new Point3D(point.X, point.Y, height + side),
-                        new Point3D(point.X + side, point.Y, height + side),
-                        new Point3D(point.X + side, point.Y + side, height),
-                        new Point3D(point.X, point.Y + side, height),
-                        new Point3D(point.X, point.Y, height),
-                        new Point3D(point.X + side, point.Y, height),
-                    },
-                    TriangleIndices = new Int32Collection()
+                        Positions = new Point3DCollection()
+                        {
+                            new Point3D(point.X + side, point.Y + side, height + side),
+                            new Point3D(point.X, point.Y + side, height + side),
+                            new Point3D(point.X, point.Y, height + side),
+                            new Point3D(point.X + side, point.Y, height + side),
+                            new Point3D(point.X + side, point.Y + side, height),
+                            new Point3D(point.X, point.Y + side, height),
+                            new Point3D(point.X, point.Y, height),
+                            new Point3D(point.X + side, point.Y, height),
+                        },
+                        TriangleIndices = new Int32Collection()
+                        {
+                            0,1,2,
+                            0,2,3,
+                            4,7,6,
+                            4,6,5,
+                            4,0,3,
+                            4,3,7,
+                            1,5,6,
+                            1,6,2,
+                            1,0,4,
+                            1,4,5,
+                            2,6,7,
+                            2,7,3
+                        }
+                    };
+                    model.Material = new DiffuseMaterial()
                     {
-                        0,1,2,
-                        0,2,3,
-                        4,7,6,
-                        4,6,5,
-                        4,0,3,
-                        4,3,7,
-                        1,5,6,
-                        1,6,2,
-                        1,0,4,
-                        1,4,5,
-                        2,6,7,
-                        2,7,3
-                    }
-                };
-                model.Material = new DiffuseMaterial()
-                {
-                    Brush = new SolidColorBrush()
-                    {
-                        Color = GetColorFromConnectivity(point.Connectivity)
-                    }
-                };
-                Map.Children.Add(model);
+                        Brush = new SolidColorBrush()
+                        {
+                            Color = GetColorFromConnectivity(point.Connectivity)
+                        }
+                    };
+                    toolTips.Add(model, point.Id + " " + point.Type + " " + point.Description);
+                    Map.Children.Add(model);
+                }
             });          
         }
 
-        private void Draw3DLines(MapNetworkModel mapNetworkModel)
+        private void Draw3DLines()
         {
             int side = 2;
             int height = 2;
@@ -158,6 +169,141 @@ namespace Project4
                 default:
                     return Color.FromRgb(0, 127, 255);
             }
+        }
+
+        private void Viewport_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Viewport.CaptureMouse();
+            start = e.GetPosition(this);
+            diffOffset.X = Translation.OffsetX;
+            diffOffset.Y = Translation.OffsetY;
+
+            Point mouseposition = e.GetPosition(Viewport);
+            Point3D testpoint3D = new Point3D(mouseposition.X, mouseposition.Y, 0);
+            Vector3D testdirection = new Vector3D(mouseposition.X, mouseposition.Y, 10);
+
+            PointHitTestParameters pointparams =
+                     new PointHitTestParameters(mouseposition);
+            RayHitTestParameters rayparams =
+                     new RayHitTestParameters(testpoint3D, testdirection);
+
+            hitGeo = null;
+            VisualTreeHelper.HitTest(Viewport, null, HTResult, pointparams);
+        }
+
+        private void Viewport_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Viewport.ReleaseMouseCapture();
+        }
+
+        private void Viewport_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point end = e.GetPosition(this);
+            double offsetX = end.X - start.X;
+            double offsetY = end.Y - start.Y;
+            double w = this.Width;
+            double h = this.Height;
+            double translateX = (offsetX * 100) / w;
+            double translateY = -(offsetY * 100) / h;
+
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                if (Viewport.IsMouseCaptured)
+                {
+                    Translation.OffsetX = diffOffset.X + (translateX * 1175 / (100 * Scaling.ScaleX));
+                    Translation.OffsetY = diffOffset.Y + (translateY * 775 / (100 * Scaling.ScaleX));
+                }
+            }
+
+            if (e.MiddleButton == MouseButtonState.Pressed)
+            { 
+                if (Viewport.IsMouseCaptured)
+                {
+                    XRotation.Angle = startAngleX + offsetY * 0.1;
+                    YRotation.Angle = startAngleY + offsetX * 0.1;
+                }
+            }
+        }
+
+        private void Viewport_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            Point p = e.MouseDevice.GetPosition(this);
+            double scaleX = 1;
+            double scaleY = 1;
+            if (e.Delta > 0 && zoomCurrent < zoomMax)
+            {
+                scaleX = Scaling.ScaleX + 0.1;
+                scaleY = Scaling.ScaleY + 0.1;
+                zoomCurrent++;
+                Scaling.ScaleX = scaleX;
+                Scaling.ScaleY = scaleY;
+            }
+            else if (e.Delta <= 0 && zoomCurrent > -zoomMax)
+            {
+                scaleX = Scaling.ScaleX - 0.1;
+                scaleY = Scaling.ScaleY - 0.1;
+                zoomCurrent--;
+                Scaling.ScaleX = scaleX;
+                Scaling.ScaleY = scaleY;
+            }
+        }
+
+        private void Viewport_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Middle && e.ButtonState == MouseButtonState.Pressed)
+            {
+                Viewport.CaptureMouse();
+                start = e.GetPosition(this);
+                startAngleX = XRotation.Angle;
+                startAngleY = YRotation.Angle;
+            }
+        }
+
+        private void Viewport_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Middle && e.ButtonState == MouseButtonState.Released)
+            {
+                Viewport.ReleaseMouseCapture();
+            }
+        }
+
+        private HitTestResultBehavior
+        HTResult(HitTestResult rawresult)
+        {
+            RayHitTestResult rayResult = rawresult as RayHitTestResult;
+
+            if (rayResult != null)
+            {
+                bool gasit = false;
+
+                try
+                {
+                    for (int i = 1; i < Map.Children.Count; i++)
+                    {
+                        GeometryModel3D model = (GeometryModel3D)Map.Children[i];
+
+                        if (model == rayResult.ModelHit)
+                        {
+                            hitGeo = (GeometryModel3D)rayResult.ModelHit;
+                            gasit = true;
+
+                            string text = toolTips[model];
+                            TooltipText.Visibility = Visibility.Visible;
+                            TooltipText.Text = text;
+                        }
+                    }
+                } catch (Exception) { }
+                
+                if (!gasit)
+                {
+                    hitGeo = null;
+
+                    TooltipText.Visibility = Visibility.Hidden;
+                    TooltipText.Text = "";
+                }
+            }
+
+            return HitTestResultBehavior.Stop;
         }
     }
 }
